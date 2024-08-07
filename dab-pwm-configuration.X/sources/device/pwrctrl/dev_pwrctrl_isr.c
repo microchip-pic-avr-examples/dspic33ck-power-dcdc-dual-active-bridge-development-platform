c4743cc5b81d2506122b39c4ffe35325be8ebaaa
M15690 <KristineAngelica.Panghulan@microchip.com>
2024-08-07 11:59:24 +0100

    added Power control loop and pin debugging to measure the CPU load: 68%

--- dab-pwm-configuration.X/sources/device/pwrctrl/dev_pwrctrl_isr.c
+++ dab-pwm-configuration.X/sources/device/pwrctrl/dev_pwrctrl_isr.c
@@ -20,6 +20,7 @@
     TERMS.
  */
 
+#include <stdbool.h>
 
 #include "dev_pwrctrl.h"
 #include "timer/sccp1.h"
@@ -29,8 +30,10 @@
 #include "dev_pwrctrl_pwm.h"
 #include "device/fault/dev_fault.h"
 #include "dcdt/dev_pwrctrl_dcdt.h"
+#include "system/pins.h"
 
 void Dev_PwrCtrl_UpdateConverterData(void);
+void Dev_PwrCtrl_ControlLoopExecute(void);
 
 /*********************************************************************************
  * @ingroup 
@@ -45,46 +48,23 @@ void Dev_PwrCtrl_UpdateConverterData(void);
  **********************************************************************************/
 void ControlLoop_Interrupt(void)
 {          
+    GPIO_1_SetHigh();
+            
     // Update the ADC data member
     Dev_PwrCtrl_UpdateConverterData();
     
     // Execute the fault detection
     Dev_Fault_Execute();
         
-    // Execute the Voltage Loop Control
-    //ToDo: Check with Lorant if the time execution of Vloop
-    if(dab.PowerDirection == PWR_CTRL_CHARGING)
-    { 
-        VMC_2p2z.KfactorCoeffsB = 0x7FFF;
-        VMC_2p2z.maxOutput =  0x7FFF;
-        
-        dab.VLoop.Feedback = dab.Adc.VSecVoltage << 4;
-        dab.VLoop.Reference = dab.VLoop.Reference << 4;
-        
-        XFT_SMPS_Controller2P2ZUpdate(&VMC_2p2z, &dab.VLoop.Feedback,
-                dab.VLoop.Reference, dab.VLoop.Output);
-    }
-    
-    // Execute the Current Loop Control
-    dab.ILoop.Feedback = dab.Adc.ISenseSecondary << 3;
-    //adaptive gain factor
-    IMC_2p2z.KfactorCoeffsB = dab.ILoop.AgcFactor;
-    //refresh limits
-    IMC_2p2z.maxOutput =  0x7FFF;
-    
-    //mixing stage from voltage loop 10khz
-    uint32_t RefBuf = (uint32_t)dab.ILoop.Reference * (uint32_t)(dab.VLoop.Output & 0x7FFF);
-    dab.ILoop.Reference = (uint16_t)(RefBuf>>12) ; //15-3
-    
-    XFT_SMPS_Controller2P2ZUpdate(&IMC_2p2z, &dab.ILoop.Feedback, 
-            dab.ILoop.Reference, &dab.ILoop.Output);    
-    
+    #if(OPEN_LOOP_PBV == false)
+    Dev_PwrCtrl_ControlLoopExecute();
+    #endif
     
     #if (true == DPDB_TEST_RUN)
 
     // Connect TP11 -> Pot1 & TP12 -> Pot2 in digital Power Development Board
-    uint16_t Pot1 = dab.Adc.ISecAverage; 
-    uint16_t Pot2 = dab.Adc.ISenseSecondary; 
+    uint16_t Pot1 = dab.Data.ISecAverage; 
+    uint16_t Pot2 = dab.Data.ISenseSecondary; 
     
     // Calculate the Frequency based on the Potentiometer 1 voltage
     dab.Pwm.ControlPeriod = (uint16_t)(MIN_PWM_PERIOD + (Pot1 * ADC_PERIOD_RANGE)); 
@@ -96,24 +76,143 @@ void ControlLoop_Interrupt(void)
        
     // Update PWM Properties
     Dev_PwrCtrl_PWM_Update(&dab);
+    
+    GPIO_1_SetLow();
+    
 }
 
 
 
-
+/*******************************************************************************
+ * @ingroup 
+ * @brief  
+ * @return 
+ * 
+ * @details 
+ * 
+ *********************************************************************************/
 void Dev_PwrCtrl_UpdateConverterData (void)
 {
+    static uint16_t ctr = 0;
+    
     // read dedicated core ADC results, these are triggered via PWM1 trigger 1
-    dab.Adc.ISecAverage = ADC1_ConversionResultGet(ISEC_AVG); // used for control
-    dab.Adc.ISenseSecondary = ADC1_ConversionResultGet(ISEC_CT); // used for protection
+    dab.Data.ISecAverage = ADC1_ConversionResultGet(ISEC_AVG); // used for control
+    dab.Data.ISenseSecondary = ADC1_ConversionResultGet(ISEC_CT); // used for protection
     
     // read shared ADC core results
     // these are all triggered by PWM1 trigger 1
-    dab.Adc.VSecVoltage = ADC1_ConversionResultGet(VSEC);
-    dab.Adc.ISensePrimary = ADC1_ConversionResultGet(IPRI_CT);   
+    dab.Data.VSecVoltage = ADC1_ConversionResultGet(VSEC);
+    dab.Data.ISensePrimary = ADC1_ConversionResultGet(IPRI_CT);   
     
-    dab.Adc.VPriVoltage = ADC1_ConversionResultGet(VPRI);
+    dab.Data.VPriVoltage = ADC1_ConversionResultGet(VPRI);
 
-    dab.Adc.VRail_5V = ADC1_ConversionResultGet(VRAIL_5V);
-    dab.Adc.Temperature = ADC1_ConversionResultGet(TEMP);
+    dab.Data.VRail_5V = ADC1_ConversionResultGet(VRAIL_5V);
+    dab.Data.Temperature = ADC1_ConversionResultGet(TEMP);
+    
+    if(ctr == 10)
+    {
+    dab.Data.SecPower = (uint16_t)(UNITS_FROM_ADC_TO_ENG(dab.Data.ISecAverage, ISEC_CT_SNS_GAIN)
+            * UNITS_FROM_ADC_TO_ENG(dab.Data.VSecVoltage, VSEC_SNS_GAIN));
+    
+    ctr = 0;
+    }
+}
+
+/*******************************************************************************
+ * @ingroup 
+ * @brief  
+ * @return 
+ * 
+ * @details 
+ * 
+ *********************************************************************************/
+void Dev_PwrCtrl_ControlLoopExecute(void)
+{
+    static uint16_t cnt = 0;
+    static bool VLoopExec = true;
+
+    //Interleave the execution of the control loop
+    if(++cnt == 5)
+    {
+        if((dab.VLoop.Enable == false) && (VLoopExec == true)){
+            dab.VLoop.Enable = true;
+            dab.PLoop.Enable = false;
+            
+        }
+        else if((dab.PLoop.Enable == false) && (VLoopExec == false)){
+            dab.VLoop.Enable = false;
+            dab.PLoop.Enable = true;
+        }
+        cnt = 0;
+    }
+    
+    if((dab.VLoop.Enable == true) && (VLoopExec == true))
+    {
+//        GPIO_1_SetHigh();
+        
+        VLoopExec = false;
+        
+        // Execute the Voltage Loop Control
+        if(dab.PowerDirection == PWR_CTRL_CHARGING)
+        { 
+            VMC_2p2z.KfactorCoeffsB = 0x7FFF;
+            VMC_2p2z.maxOutput =  0x7FFF;
+
+            //Bit-shift value used to perform input value normalization
+            //ToDo: check with Lorant if tested with at 700V, if yes, 4 shift bit 
+            //exceeds the int value of 35535.
+            dab.VLoop.Feedback = dab.Data.VSecVoltage << 4;  
+            dab.VLoop.Reference = dab.VLoop.Reference << 4;
+
+            SMPS_Controller2P2ZUpdate(&VMC_2p2z, &dab.VLoop.Feedback,
+                    dab.VLoop.Reference, dab.VLoop.Output);
+        
+        }
+            
+//        GPIO_1_SetLow();
+    }
+
+    if((dab.PLoop.Enable == true) && (VLoopExec == false))
+    {
+//        GPIO_1_SetHigh();
+        
+        VLoopExec = true;
+        
+        // Execute the Power Loop Control
+        //ToDo: check with Lorant the meaning of 100 in this code
+        //100 is a power offset
+        
+        dab.PLoop.Feedback = 100 + (dab.Data.SecPower);
+        dab.PLoop.Reference = 100 + dab.Properties.PwrReference;
+        
+        SMPS_Controller2P2ZUpdate(&PMC_2p2z, &dab.PLoop.Feedback,
+                dab.PLoop.Reference, &dab.PLoop.Output);
+        
+//        GPIO_1_SetLow();
+    }
+    
+    if(dab.ILoop.Enable == true)
+    {
+        // Execute the Current Loop Control
+        //Bit-shift value used to perform input value normalization
+        dab.ILoop.Feedback = dab.Data.ISenseSecondary << 3;
+        //adaptive gain factor
+        IMC_2p2z.KfactorCoeffsB = dab.ILoop.AgcFactor;
+        //refresh limits
+        IMC_2p2z.maxOutput =  0x7FFF;
+
+        //mixing stage from voltage loop 10KHz
+        uint32_t RefBuf = (uint32_t)dab.ILoop.Reference * (uint32_t)(dab.VLoop.Output & 0x7FFF);
+        uint16_t ILoopReference = (uint16_t)(RefBuf>>12) ; //15-3
+
+        //mixing stage from power loop 10KHz
+        RefBuf =  (uint32_t)ILoopReference * (uint32_t)(dab.PLoop.Output & 0x7FFF);  
+        ILoopReference = (int16_t)(RefBuf>>15 );    
+
+        XFT_SMPS_Controller2P2ZUpdate(&IMC_2p2z, &dab.ILoop.Feedback, 
+                ILoopReference, &dab.ILoop.Output);    
+
+        //control loop output copied to control phase
+        dab.Pwm.ControlPhase = (((dab.Pwm.ControlDutyCycle >> 1) * dab.ILoop.Output) >> 15);
+    }
 }
