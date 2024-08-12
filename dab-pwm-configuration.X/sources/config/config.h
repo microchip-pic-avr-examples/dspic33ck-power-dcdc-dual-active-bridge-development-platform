3bacdf9eb7e185809f98d9d62436b4d342cd4869
M15690 <KristineAngelica.Panghulan@microchip.com>
2024-08-12 10:30:33 +0100

    faults are all false

--- dab-pwm-configuration.X/sources/config/config.h
+++ dab-pwm-configuration.X/sources/config/config.h
@@ -19,12 +19,13 @@
 //------------------------------------------------------------------------------
 // fault disables (for debug only)
 //------------------------------------------------------------------------------
-#define FAULT_VPRI_OV   true
-#define FAULT_ISEC_OC   true
-#define FAULT_IPRI_OC   true
-#define FAULT_VSEC_OV   true
-#define FAULT_PS_OTP    true
+#define FAULT_VPRI_OV   false
+#define FAULT_ISEC_OC   false
+#define FAULT_IPRI_OC   false
+#define FAULT_VSEC_OV   false
+#define FAULT_PS_OTP    false
 
+// not implemented yet
 #define FAULT_SHORT_CCT false
 #define FAULT_VRAIL_5V  false
 
