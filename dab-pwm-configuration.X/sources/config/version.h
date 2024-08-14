81d9f686b557daf80b6dbfdbafe25becf7c93ed9
Lorant Dajka - Bedo <Lorant.Dajka-Bedo@microchip.com>
2024-08-14 11:03:04 +0200

    dev temperature first test

--- dab-pwm-configuration.X/sources/config/version.h
+++ dab-pwm-configuration.X/sources/config/version.h
@@ -37,7 +37,7 @@
 //#define FIRMWARE_VERSION (uint32_t) 0x00010002 ///< from right to left:  digits 5-n: Major,  digits 3-4: Minor,  digits 0-2: Revision, 
 #define FIRMWARE_VERSION_MAJOR    (0x00)
 #define FIRMWARE_VERSION_MINOR    (0x01)
-#define FIRMWARE_VERSION_REVISION (0x0001)
+#define FIRMWARE_VERSION_REVISION (0x0002)
 #define FIRMWARE_VERSION (uint32_t)(FIRMWARE_VERSION_MAJOR << 24)+ (uint32_t)(FIRMWARE_VERSION_MINOR<<16) + (uint32_t)(FIRMWARE_VERSION_REVISION)
 /*********************************************************************************
  * @ingroup apps-layer-properties-public-defines
@@ -51,8 +51,8 @@
  *  This string may be used to be displayed in terminal windows or LC displays.
  **********************************************************************************/
 //#define FIRMWARE_VERSION_STRING "00.01.00" ///< Clear text version number.   //  manually TRACK a quick sheet of release/dev history in comments
-#define FIRMWARE_VERSION_STRING "00.01.0001" ///< 13.08.2024 ILoop, Vloop, closed
-//#define FIRMWARE_VERSION_STRING "00.01.0002" ///< 14.08.2024 ILoop, Vloop, Ploop closed
+//#define FIRMWARE_VERSION_STRING "00.01.0001" ///< 13.08.2024 ILoop, Vloop, closed
+#define FIRMWARE_VERSION_STRING "00.01.0002" ///< 14.08.2024 ILoop, Vloop, Ploop closed
 
 /*********************************************************************************
  * @ingroup apps-layer-properties-public-defines
