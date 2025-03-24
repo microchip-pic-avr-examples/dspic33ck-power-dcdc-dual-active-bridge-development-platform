7ef106cdd9cabcdbafbe57325853a32ee1e86d84
M15690 <KristineAngelica.Panghulan@microchip.com>
2025-03-24 16:45:24 +0000

    updated the version from 1.0.0 to 2.0.0 for deployment; added feature:: reverse mode operation

--- dspic33ck-dab.X/sources/config/version.h
+++ dspic33ck-dab.X/sources/config/version.h
@@ -29,12 +29,12 @@
  *  features or limitations.
  **********************************************************************************/
 #define FIRMWARE_VERSION_MAJOR    (0x00)
-#define FIRMWARE_VERSION_MINOR    (0x01)
-//#define FIRMWARE_VERSION_REVISION (0x0000)
+#define FIRMWARE_VERSION_MINOR    (0x02)
+#define FIRMWARE_VERSION_REVISION (0x0000)
 //#define FIRMWARE_VERSION_REVISION (0x0002) //reverse/discharger mode implementation
 //#define FIRMWARE_VERSION_REVISION (0x0003) //load disconnect supress while not up and running state. Avoids false detection during ref transitions
 //#define FIRMWARE_VERSION_REVISION (0x0004) //minumum phase clamping lim increased due to glitch on startup.  Transformer Fault OC handling improved 
-#define FIRMWARE_VERSION_REVISION (0x0005)   //cleanup, comments, macros
+//#define FIRMWARE_VERSION_REVISION (0x0005)   //cleanup, comments, macros
 #define FIRMWARE_VERSION (uint32_t)(FIRMWARE_VERSION_MAJOR << 24)+ (uint32_t)(FIRMWARE_VERSION_MINOR<<16) + (uint32_t)(FIRMWARE_VERSION_REVISION)
 /*********************************************************************************
  * @ingroup apps-layer-properties-public-defines
@@ -47,7 +47,7 @@
  *  number REV.
  *  This string may be used to be displayed in terminal windows or LC displays.
  **********************************************************************************/
-#define FIRMWARE_VERSION_STRING "00.01.0005" 
+#define FIRMWARE_VERSION_STRING "00.02.0000" 
 /*********************************************************************************
  * @ingroup apps-layer-properties-public-defines
  * @brief   Start screen string of second line showing firmware name
